21d7d66e3c8911a912ea2775a1eed323fd5c1c2e
Daniel.C <cheadaniel.95@gmail.com>
2023-04-24 12:56:43 +0200

    ajout du toggle pour afficher ou non  l'annonce

--- src/Form/AnnonceFormType.php
+++ src/Form/AnnonceFormType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Annonce;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -18,6 +20,13 @@ class AnnonceFormType extends AbstractType
             ->add('Name', TextType::class)
             ->add('Description', TextType::class)
             ->add('Price', IntegerType::class)
+            ->add('is_visible',ChoiceType::class, [
+                'label'=>'Montrer l\'annonce :',
+                'choices'=> [
+                    'Visible'=>true,
+                    'Non Visible'=>false,
+                ]
+            ])
             ->add('Add', SubmitType::class)
         ;
     }
